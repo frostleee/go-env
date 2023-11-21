c2cdafbe721ea5631df58bd631f5a9b1afaf2dd0
Sergey Vakhrushev <sergey.vakhrushev@devim.team>
2023-11-21 19:30:35 +0400

    add cache and container

--- env.go
+++ env.go
@@ -5,59 +5,83 @@ import (
 	"strconv"
 )
 
-type Environment struct {
+type Env struct {
 	key    string
 	value  string
 	exists bool
 }
 
-func Get(key string) *Environment {
-	env := &Environment{key: key}
+func NewEnv(key string) *Env {
+	env := &Env{key: key}
 	env.value, env.exists = os.LookupEnv(env.key)
-
 	return env
 }
 
-func (env *Environment) String(def string) string {
-	if env.value == "" {
+func (env *Env) String(def string) string {
+	if !env.exists {
 		return def
 	}
-
 	return env.value
 }
 
-func (env *Environment) Int(def int) int {
-	value, err := strconv.Atoi(env.value)
-	if err != nil {
+func (env *Env) Int(def int) int {
+	if !env.exists {
 		return def
 	}
 
-	return value
+	if value, err := strconv.Atoi(env.value); err == nil {
+		return value
+	}
+	return def
 }
 
-func (env *Environment) Bool(def bool) bool {
-	value, err := strconv.ParseBool(env.value)
-	if err != nil {
+func (env *Env) Bool(def bool) bool {
+	if !env.exists {
 		return def
 	}
 
-	return value
+	if value, err := strconv.ParseBool(env.value); err == nil {
+		return value
+	}
+	return def
 }
 
-func (env *Environment) Float64(def float64) float64 {
-	value, err := strconv.ParseFloat(env.value, 64)
-	if err != nil {
+func (env *Env) Float64(def float64) float64 {
+	if !env.exists {
 		return def
 	}
 
-	return value
+	if value, err := strconv.ParseFloat(env.value, 64); err == nil {
+		return value
+	}
+	return def
 }
 
-func (env *Environment) Float32(def float32) float32 {
-	value, err := strconv.ParseFloat(env.value, 32)
-	if err != nil {
+func (env *Env) Float32(def float32) float32 {
+	if !env.exists {
 		return def
 	}
 
-	return float32(value)
+	if value, err := strconv.ParseFloat(env.value, 32); err == nil {
+		return float32(value)
+	}
+	return def
+}
+
+type Environment struct {
+	envMap map[string]*Env
+}
+
+func NewEnvironment() *Environment {
+	return &Environment{
+		envMap: make(map[string]*Env),
+	}
+}
+
+func (env *Environment) GetEnv(key string) *Env {
+	if env, ok := env.envMap[key]; ok {
+		return env
+	}
+
+	return &Env{}
 }
--- env_test.go
+++ env_test.go
@@ -1,99 +1,312 @@
 package env
 
 import (
-	"github.com/stretchr/testify/assert"
 	"os"
-	"strconv"
 	"testing"
 )
 
 const (
-	EnvStringName         = "ENV_STRING"
-	EnvStringValue        = "value"
-	EnvStringDefaultValue = "default_value"
-
-	EnvIntName         = "ENV_INT"
-	EnvIntValue        = "123"
-	EnvIntDefaultValue = 321
-
 	EnvBoolName         = "ENV_BOOL"
 	EnvBoolValue        = "true"
 	EnvBoolDefaultValue = false
+)
 
-	EnvFloat64Name         = "ENV_FLOAT64"
-	EnvFloat64Value        = "3.141592653589793"
-	EnvFloat64DefaultValue = 0.3333333333333333
+func TestEnv_Bool(t *testing.T) {
+	type fields struct {
+		key    string
+		value  string
+		exists bool
+	}
+	type args struct {
+		def bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			name: "exists",
+			fields: fields{
+				key:    EnvBoolName,
+				value:  EnvBoolValue,
+				exists: true,
+			},
+			args: args{
+				def: EnvBoolDefaultValue,
+			},
+			want: true,
+		},
+		{
+			name: "not exists",
+			fields: fields{
+				key:    EnvBoolName,
+				value:  EnvBoolValue,
+				exists: false,
+			},
+			args: args{
+				def: EnvBoolDefaultValue,
+			},
+			want: EnvBoolDefaultValue,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := &Env{
+				key:    tt.fields.key,
+				value:  tt.fields.value,
+				exists: tt.fields.exists,
+			}
+			if got := env.Bool(tt.args.def); got != tt.want {
+				t.Errorf("Bool() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
+const (
 	EnvFloat32Name         = "ENV_FLOAT32"
 	EnvFloat32Value        = "3.1415927"
 	EnvFloat32DefaultValue = 0.333
 )
 
-func TestEnvironment_String(t *testing.T) {
-	os.Setenv(EnvStringName, EnvStringValue)
-	defer os.Unsetenv(EnvStringName)
-
-	actual := Get(EnvStringName).String(EnvStringDefaultValue)
-	assert.Equal(t, EnvStringValue, actual)
+func TestEnv_Float32(t *testing.T) {
+	type fields struct {
+		key    string
+		value  string
+		exists bool
+	}
+	type args struct {
+		def float32
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   float32
+	}{
+		{
+			name: "exists",
+			fields: fields{
+				key:    EnvFloat32Name,
+				value:  EnvFloat32Value,
+				exists: true,
+			},
+			args: args{
+				def: EnvFloat32DefaultValue,
+			},
+			want: 3.1415927,
+		},
+		{
+			name: "not exists",
+			fields: fields{
+				key:    EnvFloat32Name,
+				value:  EnvFloat32Value,
+				exists: false,
+			},
+			args: args{
+				def: EnvFloat32DefaultValue,
+			},
+			want: EnvFloat32DefaultValue,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := &Env{
+				key:    tt.fields.key,
+				value:  tt.fields.value,
+				exists: tt.fields.exists,
+			}
+			if got := env.Float32(tt.args.def); got != tt.want {
+				t.Errorf("Float32() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
-func TestEnvironment_String_Default(t *testing.T) {
-	actual := Get(EnvStringName).String(EnvStringDefaultValue)
-	assert.Equal(t, EnvStringDefaultValue, actual)
+const (
+	EnvFloat64Name         = "ENV_FLOAT64"
+	EnvFloat64Value        = "3.141592653589793"
+	EnvFloat64DefaultValue = 0.3333333333333333
+)
+
+func TestEnv_Float64(t *testing.T) {
+	type fields struct {
+		key    string
+		value  string
+		exists bool
+	}
+	type args struct {
+		def float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   float64
+	}{
+		{
+			name: "exists",
+			fields: fields{
+				key:    EnvFloat64Name,
+				value:  EnvFloat64Value,
+				exists: true,
+			},
+			args: args{
+				def: EnvFloat64DefaultValue,
+			},
+			want: 3.141592653589793,
+		},
+		{
+			name: "not exists",
+			fields: fields{
+				key:    EnvFloat64Name,
+				value:  EnvFloat64Value,
+				exists: false,
+			},
+			args: args{
+				def: EnvFloat64DefaultValue,
+			},
+			want: EnvFloat64DefaultValue,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := &Env{
+				key:    tt.fields.key,
+				value:  tt.fields.value,
+				exists: tt.fields.exists,
+			}
+			if got := env.Float64(tt.args.def); got != tt.want {
+				t.Errorf("Float64() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
-func TestEnvironment_Int(t *testing.T) {
+const (
+	EnvIntName         = "ENV_INT"
+	EnvIntValue        = "123"
+	EnvIntDefaultValue = 321
+)
+
+func TestEnv_Int(t *testing.T) {
 	os.Setenv(EnvIntName, EnvIntValue)
 	defer os.Unsetenv(EnvIntName)
 
-	actual := Get(EnvIntName).Int(EnvIntDefaultValue)
-	convertIntValue, _ := strconv.Atoi(EnvIntValue)
-	assert.Equal(t, convertIntValue, actual)
+	type fields struct {
+		key    string
+		value  string
+		exists bool
+	}
+	type args struct {
+		def int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   int
+	}{
+		{
+			name: "exists",
+			fields: fields{
+				key:    EnvIntName,
+				value:  EnvIntValue,
+				exists: true,
+			},
+			args: args{
+				def: EnvIntDefaultValue,
+			},
+			want: 123,
+		},
+		{
+			name: "not exists",
+			fields: fields{
+				key:    EnvIntName,
+				value:  EnvIntValue,
+				exists: false,
+			},
+			args: args{
+				def: EnvIntDefaultValue,
+			},
+			want: EnvIntDefaultValue,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := &Env{
+				key:    tt.fields.key,
+				value:  tt.fields.value,
+				exists: tt.fields.exists,
+			}
+			if got := env.Int(tt.args.def); got != tt.want {
+				t.Errorf("Int() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
 
-func TestEnvironment_Int_Default(t *testing.T) {
-	actual := Get(EnvIntName).Int(EnvIntDefaultValue)
-	assert.Equal(t, EnvIntDefaultValue, actual)
-}
+const (
+	EnvStringName         = "ENV_STRING"
+	EnvStringValue        = "value"
+	EnvStringDefaultValue = "default_value"
+)
 
-func TestEnvironment_Bool(t *testing.T) {
-	os.Setenv(EnvBoolName, EnvBoolValue)
-	defer os.Unsetenv(EnvBoolName)
+func TestEnv_String(t *testing.T) {
+	os.Setenv(EnvStringName, EnvStringValue)
+	defer os.Unsetenv(EnvStringName)
 
-	actual := Get(EnvBoolName).Bool(EnvBoolDefaultValue)
-	convertBoolValue, _ := strconv.ParseBool(EnvBoolValue)
-	assert.Equal(t, convertBoolValue, actual)
-}
-
-func TestEnvironment_Bool_Default(t *testing.T) {
-	actual := Get(EnvBoolName).Bool(EnvBoolDefaultValue)
-	assert.Equal(t, EnvBoolDefaultValue, actual)
-}
-
-func TestEnvironment_Float64(t *testing.T) {
-	os.Setenv(EnvFloat64Name, EnvFloat64Value)
-	defer os.Unsetenv(EnvFloat64Name)
-
-	actual := Get(EnvFloat64Name).Float64(EnvFloat64DefaultValue)
-	convertFloatValue, _ := strconv.ParseFloat(EnvFloat64Value, 64)
-	assert.Equal(t, convertFloatValue, actual)
-}
-
-func TestEnvironment_Float64_Default(t *testing.T) {
-	actual := Get(EnvFloat64Name).Float64(EnvFloat64DefaultValue)
-	assert.Equal(t, EnvFloat64DefaultValue, actual)
-}
-
-func TestEnvironment_Float32(t *testing.T) {
-	os.Setenv(EnvFloat32Name, EnvFloat32Value)
-	defer os.Unsetenv(EnvFloat32Name)
-
-	actual := Get(EnvFloat32Name).Float32(float32(EnvFloat32DefaultValue))
-	convertFloatValue, _ := strconv.ParseFloat(EnvFloat32Value, 32)
-	assert.Equal(t, float32(convertFloatValue), actual)
-}
-
-func TestEnvironment_Float32_Default(t *testing.T) {
-	actual := Get(EnvFloat32Name).Float32(float32(EnvFloat32DefaultValue))
-	assert.Equal(t, float32(EnvFloat32DefaultValue), actual)
+	type fields struct {
+		key    string
+		value  string
+		exists bool
+	}
+	type args struct {
+		def string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name: "exists",
+			fields: fields{
+				key:    EnvStringName,
+				value:  EnvStringValue,
+				exists: true,
+			},
+			args: args{
+				def: EnvStringDefaultValue,
+			},
+			want: EnvStringValue,
+		},
+		{
+			name: "default",
+			fields: fields{
+				key:    EnvStringName,
+				value:  EnvStringValue,
+				exists: false,
+			},
+			args: args{
+				def: EnvStringDefaultValue,
+			},
+			want: EnvStringDefaultValue,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := &Env{
+				key:    tt.fields.key,
+				value:  tt.fields.value,
+				exists: tt.fields.exists,
+			}
+			if got := env.String(tt.args.def); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
